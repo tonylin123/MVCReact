07f4711f27fc959bbcb99f517dd04fa8cdabbfd2
Elev <Elev@DESKTOP-QA297QM>
2022-11-14 14:55:45 +0100

    new

--- MVCData/Program.cs
+++ MVCData/Program.cs
@@ -1,8 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddMvc();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,7 +19,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
