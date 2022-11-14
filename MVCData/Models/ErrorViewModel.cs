0bf65b73aa8a589216852c5c2d5635d905dfac62
Elev <Elev@DESKTOP-QA297QM>
2022-11-14 14:35:34 +0100

    Add project files.

---
+++ MVCData/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCData.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
