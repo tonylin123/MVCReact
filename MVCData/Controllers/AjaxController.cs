0d143db64907c7cb27f27314d7982b022bb70e12
Elev <Elev@DESKTOP-QA297QM>
2022-11-23 13:28:02 +0100

    new

--- MVCData/Controllers/AjaxController.cs
+++ MVCData/Controllers/AjaxController.cs
@@ -8,38 +8,43 @@ namespace MVCData.Controllers
     {
         public IActionResult Index()
         {
+            
             return View();
         }
 
 
-        public IActionResult GetPeople()
+        public IActionResult PeopleList()
         {
             PeopleViewModel viewModel = PeopleModel.List();
-            return View("_personPartial", viewModel);
+            return View("_PeopleList", viewModel);
         }
 
         [HttpPost]
         public IActionResult DeletePerson(string id)
         {
+            
             var personToDelete = PeopleData.List.FirstOrDefault(p => p.Id == id);
 
             if (personToDelete != null)
             {
                 PeopleData.List.Remove(personToDelete);
+                return Json(id + " was deleted, status: " + StatusCode(200).StatusCode);
             }
 
-            return RedirectToAction("GetPeople");
-       
+            return Json("Error could not delete, status: " + StatusCode(400).StatusCode);
+
         }
         [HttpPost]
         public IActionResult GetDetails(string id)
         {
+            
             Person person = PeopleData.List.FirstOrDefault(x => x.Id == id);
+
             if (person == null)
             {
                 ViewBag.ERROR = "Person not found";
             }
-            return PartialView("_personPartial", person);
+            return View("_personPartial", person);
         }
 
 
