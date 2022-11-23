bc3d098948333f6d1f0807ee1468f5ef81c77ab6
Elev <Elev@DESKTOP-QA297QM>
2022-11-23 15:27:20 +0100

    test

--- MVCData/ViewModels/PeopleData.cs
+++ MVCData/ViewModels/PeopleData.cs
@@ -7,7 +7,7 @@
 
         public static List<Person> List { get; set; } = new List<Person>()
         {
-           new Person(Guid.NewGuid().ToString(),"Jake Paul", "1232", "ManCity"),
+           new Person(Guid.NewGuid().ToString(),"Jake Paul", "123289", "ManCity"),
            new Person(Guid.NewGuid().ToString(), "Tony Stark", "12345", "Gothenburg"),
            new Person(Guid.NewGuid().ToString(), "Captain America", "3213213", "NewYork"),
            new Person(Guid.NewGuid().ToString(), "Flash Man", "312312", "Washington"),
